dfb3f81f8c5d9c2d82c036c8233408dc0e64278c
guhaotian <guhaotianwufan@gmail.com>
2015-11-24 11:51:55 -0600

    fix a problem

--- install-webserver.sh
+++ install-webserver.sh
@@ -4,10 +4,10 @@ sudo apt-get update -y
 sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql 
 
 git clone https://github.com/guhaotian/mp1-php.git
-mv ./mp1/gallery.php /var/www/html
-mv ./mp1/index.php /var/www/html
-mv ./mp1/submit.php /var/www/html
-mv ./mp1/setup.php /var/www/html
+mv ./mp1-php/gallery.php /var/www/html
+mv ./mp1-php/index.php /var/www/html
+mv ./mp1-php/submit.php /var/www/html
+mv ./mp1-php/setup.php /var/www/html
 
 
 curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
